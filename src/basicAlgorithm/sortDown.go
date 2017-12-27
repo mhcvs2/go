de89f1a324a7499e8b4e71df0d8a8082236317de
mhcvs2 <merzhong0501@163.com>
2017-12-28 00:50:00 +0800

    add

--- src/basicAlgorithm/sortDown.go
+++ src/basicAlgorithm/sortDown.go
@@ -8,15 +8,15 @@ func init() {
 	data = []int{3,1,5,7,2,8,4,9,6}
 }
 
-func insertSort() {
+func insertSortDown() {
 	l := len(data)
 	for i:=1; i<l; i++ {
-		if data[i] < data[i-1] {
+		if data[i] > data[i-1] {
 			guard := data[i]
-			j := i-1
 			data[i] = data[i-1]
+			j := i - 1
 			for {
-				if j<0 || guard >= data[j] {
+				if j < 0 || guard <= data[j] {
 					break
 				}
 				data[j+1] = data[j]
@@ -27,20 +27,19 @@ func insertSort() {
 	}
 }
 
-func bubbleSort() {
+func bubbleSortDown() {
 	l := len(data)
-	for i:=0; i<l; i++ {
-		for j:=i; j<l; j++{
-			if data[j] < data[i] {
-				data[j], data[i] = data[i], data[j]
+	for i:=0; i<l; i++{
+		for j:=i;j<l; j++{
+			if data[j] > data[i] {
+				data[i], data[j] = data[j], data[i]
 			}
 		}
 	}
 }
 
-
 func main() {
-	//insertSort()
-	bubbleSort()
+	//insertSortDown()
+	bubbleSortDown()
 	fmt.Println(data)
 }
