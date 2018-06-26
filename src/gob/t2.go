a2f8414380f18fbd8abd5f00cec6e1f181231cee
mhcvs2 <merzhong0501@163.com>
2018-06-26 23:01:57 +0800

    add

--- src/gob/t2.go
+++ src/gob/t2.go
@@ -13,15 +13,28 @@ type User struct {
 	Name, Pass string
 }
 
-func main() {
-	var datato = &User{"Donald", "DuckPass"}
+func t1() {
+	var datato = &User{"Donald1", "DuckPass1"}
 	var datafrom = new(User)
 	err := Save(file, datato)
 	Check(err)
 	err = Load(file, datafrom)
 	Check(err)
 	fmt.Println(datafrom)
+}
 
+func t2() {
+	var datato = &User{"Donal22d", "22"}
+	var datafrom = new(User)
+	err := Save2(file, datato)
+	Check(err)
+	err = Load(file, datafrom)
+	Check(err)
+	fmt.Println(datafrom)
+}
+
+func main() {
+	t1()
 }
 
 func Save(path string, object interface{}) error {
@@ -29,6 +42,18 @@ func Save(path string, object interface{}) error {
 	if err == nil {
 		encoder := gob.NewEncoder(file)
 		encoder.Encode(object)
+	}
+	file.Close()
+	return err
+}
+
+func Save2(path string, object interface{}) error {
+	file, err := os.Open(path)
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(object)
+	} else {
+		panic(err)
 	}
 	file.Close()
 	return err
