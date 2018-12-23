c036e40beebcb084679fd0398f73b4f770ebd899
MAHONGCHAO [马洪超] <MAHONGCHAO@kingsoft.cn>
2018-12-23 15:30:58 +0800

    add

--- src/orm_test/gorm_test/mysql/main.go
+++ src/orm_test/gorm_test/mysql/main.go
@@ -13,7 +13,7 @@ type Product struct {
 }
 
 func t1() {
-	db, err := gorm.Open("mysql", "root:root.123@tcp(services.gcloud.srcb.com:30306)/test?charset=utf8&parseTime&loc=Local")
+	db, err := gorm.Open("mysql", "root:123@tcp(ali:3306)/gorm_test?charset=utf8&parseTime=true&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	} else {
@@ -30,18 +30,19 @@ func t1() {
 	// Read
 	var product Product
 	db.First(&product, 1) // find product with id 1
-	fmt.Println(product.Code)
+	fmt.Println("product.Code: ", product.Code)
 	db.First(&product, "code = ?", "L1212") // find product with code l1212
-	fmt.Println(product.Code)
+	fmt.Println("product.Code: ", product.Code)
 	// Update - update product's price to 2000
+	fmt.Println("update")
 	db.Model(&product).Update("Price", 2000)
-
+	fmt.Println("delete")
 	// Delete - delete product
 	db.Delete(&product)
 }
 
 func t2() {
-	db, err := gorm.Open("mysql", "root:root.123@tcp(services.gcloud.srcb.com:30306)/test?charset=utf8&parseTime&loc=Local")
+	db, err := gorm.Open("mysql", "root:123@tcp(ali:3306)/gorm_test?charset=utf8&parseTime=true&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	} else {
@@ -51,10 +52,26 @@ func t2() {
 
 	// Read
 	var product Product
-	db.First(&product, 1) // find product with id 1
-	fmt.Println(product.Code)
+	db.First(&product) // find product with id 1
+	fmt.Println("product.Code: ", product.Code)
+}
+
+func t3() {
+	db, err := gorm.Open("mysql", "root:123@tcp(ali:3306)/gorm_test?charset=utf8&parseTime=true&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	} else {
+		fmt.Println("connect success")
+	}
+	defer db.Close()
+
+	// Read
+	var product Product
+	db.Where("code = ?", "L1212").First(&product)
+	fmt.Println("product.Price: ", product.Price)
+	fmt.Println("product.Code: ", product.Code)
 }
 
 func main() {
-	t2()
+	t3()
 }
